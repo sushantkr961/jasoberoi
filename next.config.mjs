f05ad2940c8014c1f457e46f197fc2cdd4bcff40
Sushant Kumar <sushantonly961@gmail.com>
2024-05-09 21:11:17 +0530

    initial setup done

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "aaah0mnbncqtinas.public.blob.vercel-storage.com",
+      "jasoberoi.ca",
+    ],
+  },
+};
+
+export default nextConfig;
---
+++ src/app/layout.js
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Footer from "@/components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Jas Oberoi | Best Realtor Surrey | White Rock | South Surrey",
+  description: "Developed by Sushant Kumar",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
