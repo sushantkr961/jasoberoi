3772b0aa49da8a5fb9d77b00afc1d12befbafce0
parag0712 <paragvadgama123@gmail.com>
2024-05-14 12:19:00 +0530

    buyer guide  complete

--- src/app/buyers-guide/page.tsx
+++ src/app/buyers-guide/page.tsx
@@ -28,7 +28,7 @@ const buyersGuide = (props: Props) => {
       <Container className="m-auto flex flex-col items-center px-2"
       >
         <div className="heading flex flex-col items-center justify-center py-10 md:py-20 sm:px-20 lg:px-24 gap-3 md:gap-8 lg:gap-10  lg:max-w-full" >
-          <h2 className="font-poppins text-[25px] md:text-[46px] uppercase leading-[1.3em] font-[300] text-center"><strong className="font-[600] ">A HASSLE FREE</strong>GUIDE TO BUYING</h2>
+          <h2 className="font-poppins text-[25px] md:text-[46px] uppercase leading-[1.3em] font-[300] text-center"><strong className="font-[600] ">A HASSLE FREE</strong> GUIDE TO BUYING</h2>
           <p className="text-[#2d2d2d] text-[15xpx] md:text-[22px] font-poppins text-center">Buying a home can be one of the grandest and happiest decisions of your life. It can also be one of the most complex and demanding decisions. To ready you for the upcoming transition, here is a general overview of what to expect and prepare.</p>
         </div>
 
