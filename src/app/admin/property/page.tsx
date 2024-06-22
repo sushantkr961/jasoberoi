b39993277656f7f4f4ab18f2cb695b727dc1aaff
Sushant Kumar <sushantonly961@gmail.com>
2024-06-23 01:11:30 +0530

    bug fix

--- src/app/admin/property/page.tsx
+++ src/app/admin/property/page.tsx
@@ -153,7 +153,7 @@ const page = () => {
                       </div>
 
                       <Link
-                        href="/admin/add-blog"
+                        href="/admin/add-property"
                         className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
                       >
                         <svg
