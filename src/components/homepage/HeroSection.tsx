bbd3d811df6e8b2f2e508a7dd6a1f2b3676b9a8a
parag0712 <paragvadgama123@gmail.com>
2024-06-29 01:51:03 +0530

    hero section

--- src/components/homepage/HeroSection.tsx
+++ src/components/homepage/HeroSection.tsx
@@ -6,13 +6,23 @@ const HeroSection = (props: Props) => {
   return (
     <section
       className="relative  pt-16 pb-4 overflow-hidden sm:pt-[7rem] sm:pb-[2rem] md:pb-0 md:py-24 min-h-full lg:min-h-[470px]  xl:min-h-[670px] w-full flex items-center justify-center "
-      style={{
-        backgroundImage: "url('/assets/webp/background.webp')",
-        backgroundPosition: "top center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "100% auto",
-      }}
+      // style={{
+      //   backgroundImage: "url('/assets/webp/background.webp')",
+      //   backgroundPosition: "top center",
+      //   backgroundRepeat: "no-repeat",
+      //   backgroundSize: "100% auto",
+      // }}
     >
+      <Image
+        src={"/assets/webp/background.webp"}
+        alt="JASOBEROI"
+        height={1000}
+        width={1000}
+        className="w-full object-cover absolute top-0 z-[-1]"
+      >
+
+      </Image>
+      
       <div className="min-w-full h-auto flex justify-center items-center p-[10px]">
         <div className="text-center">
           <div className="md:mb-8">
