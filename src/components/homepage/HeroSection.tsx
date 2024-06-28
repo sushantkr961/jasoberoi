c1adcd950c1224cdff981cbf009b7ff06a3d45f4
parag0712 <paragvadgama123@gmail.com>
2024-06-29 01:55:20 +0530

    hero section

--- src/components/homepage/HeroSection.tsx
+++ src/components/homepage/HeroSection.tsx
@@ -6,23 +6,23 @@ const HeroSection = (props: Props) => {
   return (
     <section
       className="relative  pt-16 pb-4 overflow-hidden sm:pt-[7rem] sm:pb-[2rem] md:pb-0 md:py-24 min-h-full lg:min-h-[470px]  xl:min-h-[670px] w-full flex items-center justify-center "
-      // style={{
-      //   backgroundImage: "url('/assets/webp/background.webp')",
-      //   backgroundPosition: "top center",
-      //   backgroundRepeat: "no-repeat",
-      //   backgroundSize: "100% auto",
-      // }}
+    // style={{
+    //   backgroundImage: "url('/assets/webp/background.webp')",
+    //   backgroundPosition: "top center",
+    //   backgroundRepeat: "no-repeat",
+    //   backgroundSize: "100% auto",
+    // }}
     >
       <Image
         src={"/assets/webp/background.webp"}
-        alt="JASOBEROI"
-        height={1000}
-        width={1000}
-        className="w-full object-cover absolute top-0 z-[-1]"
+        alt="JASOBEROI" layout="fill" // Ensures the image takes up the entire parent div's dimensions
+        objectFit="cover" // Adjust as per your design needs
+        loading="lazy"
+      className="z-[-1]"
       >
 
       </Image>
-      
+
       <div className="min-w-full h-auto flex justify-center items-center p-[10px]">
         <div className="text-center">
           <div className="md:mb-8">
@@ -31,7 +31,6 @@ const HeroSection = (props: Props) => {
               alt="JASOBEROI"
               width={768}
               height={128}
-              loading="lazy"
               className="block max-w-full"
             />
           </div>
