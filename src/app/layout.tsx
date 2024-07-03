473f6e09a7182bf0ca08e3d200038bd03bbb07b3
parag0712 <paragvadgama123@gmail.com>
2024-07-03 12:11:20 +0530

    google tag manger added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "react-hot-toast";
 import UserCheckProvider from "./UserCheckProvider";
 import "./globals.css";
 import { Metadata, Viewport } from "next";
-
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export const viewport: Viewport = {
   width: "device-width",
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
   },
 };
 // const belleza = Belleza({ subsets: ["latin"], variable: '--font-belleza', weight: '400' });
-const inter = Inter({ subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 // const crimsonPro = Crimson_Pro({ subsets: ["latin"], variable: '--font-crimson-pro', weight: ['200', '900'] });
 // const crimsonText = Crimson_Text({ subsets: ["latin"], variable: '--font-crimson-text', weight: ['400', '600', '700'] });
 // const poppins = Poppins({ subsets: ["latin"], variable: '--font-poppins', weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] });
@@ -59,19 +59,7 @@ export default function RootLayout({
   return (
     // <html lang="en" className={`${inter.variable} ${crimsonPro.variable} ${crimsonText.variable} ${poppins.variable} ${lato.variable} ${greatVibes.variable} ${belleza.variable}`}>
     <html lang="en">
-
       <head>
-
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,200..900;1,200..900&family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Great+Vibes&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap"
-          rel="stylesheet"
-        /> */}
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
@@ -92,22 +80,6 @@ export default function RootLayout({
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
         /> */}
 
-        <script
-          id="fb-pixel"
-          dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '224717419404384');
-fbq('track', 'PageView');
-`,
-          }}
-        />
       </head>
       <body
         className={cn("relative", inter.className, {
@@ -121,6 +93,28 @@ fbq('track', 'PageView');
         <UserCheckProvider>
           {children}
         </UserCheckProvider>
+
+        
+      <script
+        id="fb-pixel"
+        dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '224717419404384');
+fbq('track', 'PageView');
+`,
+        }}
+      />
+
+<GoogleTagManager gtmId="G-92EMKWF7BP" />
+
+
       </body>
     </html>
   );
